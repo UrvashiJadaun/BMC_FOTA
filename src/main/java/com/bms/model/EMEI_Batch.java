e7814260457c23d7f34ae5b2bb63cf9df6ace841
Urvashi <Urvashi@DESKTOP-264L670>
2020-08-14 11:55:05 +0530

    Second commit

--- src/main/java/com/bms/model/EMEI_Batch.java
+++ src/main/java/com/bms/model/EMEI_Batch.java
@@ -1,5 +1,18 @@
 package com.bms.model;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class EMEI_Batch {
 
+	    @CsvBindByName
+	    private long id;
+	    @CsvBindByName
+	    private String name;
+	    @CsvBindByName
+	    private String email;
+	    @CsvBindByName(column = "country")
+	    private String countryCode;
+	    @CsvBindByName
+	    private int age;
+	
 }
